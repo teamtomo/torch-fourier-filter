4976fe7270f3b73e7ffc7c0c6157b3b4e4ba8652
jdickerson95 <jdickerson@mrc-lmb.cam.ac.uk>
2024-12-05 13:03:21 -0800

    feat: Whitening for batches of micrographs + allow different output size

--- src/torch_fourier_filter/whitening.py
+++ src/torch_fourier_filter/whitening.py
@@ -16,7 +16,7 @@ def gaussian_smoothing(
     tensor: torch.Tensor, kernel_size: int = 5, sigma: float = 1.0
 ) -> torch.Tensor:
     """
-    Apply Gaussian smoothing to a 1D tensor.
+    Apply Gaussian smoothing to a 1D tensor or batch.
 
     Parameters
     ----------
@@ -42,40 +42,46 @@ def gaussian_smoothing(
     kernel = torch.exp(-0.5 * (x / sigma) ** 2)
     kernel = kernel / kernel.sum()
 
-    # Debug: Print the kernel
-    print("Gaussian kernel:", kernel)
-
     # Reshape kernel for convolution
     kernel = einops.rearrange(kernel, "k -> 1 1 k")
 
+    # Add batch and channel dimensions if necessary
+    dim_input_tensor = tensor.dim()
+    if dim_input_tensor == 1:
+        tensor = einops.rearrange(tensor, "n -> 1 1 n")
+    else:
+        tensor = einops.rearrange(tensor, "b n -> b 1 n")
+
     # Apply the Gaussian kernel to the tensor
-    tensor = einops.rearrange(tensor, "n -> 1 1 n")  # Add batch and channel dimensions
     smoothed_tensor = F.conv1d(tensor, kernel, padding=kernel_size // 2)
 
-    # Debug: Print the smoothed tensor
-    print("Smoothed tensor:", smoothed_tensor.view(-1))
-
-    return smoothed_tensor.view(-1)
+    if dim_input_tensor == 2:
+        return einops.rearrange(smoothed_tensor, "b 1 n -> b n")
+    else:
+        return einops.rearrange(smoothed_tensor, "1 1 n -> n")
 
 
 def whitening_filter(
-    image_dft: tuple[int, int] | tuple[int, int, int],
+    image_dft: torch.Tensor,
     image_shape: tuple[int, int] | tuple[int, int, int],
+    output_shape: tuple[int, int] | tuple[int, int, int],
     rfft: bool = True,
     fftshift: bool = False,
     dimensions_output: int = 2,  # 1/2/3D filter
     smoothing: bool = False,
     power_spec: bool = True,
-) -> torch.tensor:
+) -> torch.Tensor:
     """
     Create a whitening filter from an image DFT.
 
     Parameters
     ----------
     image_dft: torch.Tensor
-        The DFT of the image.
+        The DFT of the images/volumes
     image_shape: tuple[int, ...]
         Shape of the input image
+    output_shape: tuple[int, ...]
+        Shape of the output filter
     rfft: bool
         Whether the input is from an rfft (True) or full fft (False)
     fftshift: bool
@@ -92,7 +98,7 @@ def whitening_filter(
     torch.Tensor
         Whitening filter
     """
-    power_spectrum = torch.absolute(image_dft)
+    power_spectrum = torch.abs(image_dft)
     if power_spec:
         power_spectrum = power_spectrum**2
     radial_average = None
@@ -124,25 +130,30 @@ def whitening_filter(
 
     # put back to 2 or 3D if necessary
     if dimensions_output == 2:
-        if len(power_spectrum.shape) > len(image_shape):
-            image_shape = (*power_spectrum.shape[:-2], *image_shape[-2:])
+        if len(power_spectrum.shape) > len(output_shape):
+            output_shape = (*power_spectrum.shape[:-2], *output_shape[-2:])
         whiten_filter = _1d_to_rotational_average_2d_dft(
             values=radial_average,
-            image_shape=image_shape,
+            image_shape=output_shape,
             rfft=rfft,
             fftshifted=fftshift,
         )
     elif dimensions_output == 3:
-        if len(power_spectrum.shape) > len(image_shape):
-            image_shape = (*power_spectrum.shape[:-3], *image_shape[-3:])
+        if len(power_spectrum.shape) > len(output_shape):
+            output_shape = (*power_spectrum.shape[:-3], *output_shape[-3:])
         whiten_filter = _1d_to_rotational_average_3d_dft(
             values=radial_average,
-            image_shape=image_shape,
+            image_shape=output_shape,
             rfft=rfft,
             fftshifted=fftshift,
         )
 
     # Normalize the filter
-    whiten_filter /= whiten_filter.max()
+    if len(whiten_filter.shape) > dimensions_output:  # then batched
+        whiten_max = einops.reduce(whiten_filter, "b ... -> b", "max")
+        whiten_max = einops.rearrange(whiten_max, "b -> b 1 1")
+        whiten_filter /= whiten_max
+    else:
+        whiten_filter /= whiten_filter.max()
 
     return whiten_filter
--- tests/whitening/test_whitening.py
+++ tests/whitening/test_whitening.py
@@ -12,6 +12,7 @@ def test_whitening_filter():
     result = whitening_filter(
         image_dft,
         image_shape,
+        image_shape,
         rfft=False,
         fftshift=False,
         dimensions_output=2,
@@ -29,6 +30,7 @@ def test_whitening_filter():
 
     result = whitening_filter(
         image_dft,
+        image_shape,
         image_shape,
         rfft=False,
         fftshift=False,
@@ -49,6 +51,7 @@ def test_whitening_filter():
     result = whitening_filter(
         image_dft,
         image_shape,
+        image_shape,
         rfft=False,
         fftshift=False,
         dimensions_output=2,
@@ -59,6 +62,26 @@ def test_whitening_filter():
         result
     ).all(), "Test case 3 failed: Non-finite values in result"
     assert result.max() == 1.0, "Test case 3 failed: Result not normalized"
+
+    # Test case 4: Basic functionality with batched 2D input
+    image_shape = (100, 16, 16)
+    image = torch.rand(image_shape, dtype=torch.float32)
+    image_dft = torch.fft.fftn(image, dim=(-2, -1))
+
+    result = whitening_filter(
+        image_dft,
+        image_shape=image_shape[-2:],
+        output_shape=image_shape[-2:],
+        rfft=False,
+        fftshift=False,
+        dimensions_output=2,
+        smoothing=False,
+    )
+    assert result.shape == image_shape, "Test case 4 failed: Shape mismatch"
+    assert torch.isfinite(
+        result
+    ).all(), "Test case 4 failed: Non-finite values in result"
+    assert result.max() == 1.0, "Test case 4 failed: Result not normalized"
 
 
 def test_gaussian_smoothing():
