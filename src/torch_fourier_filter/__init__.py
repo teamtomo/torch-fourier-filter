6cb78bd5efc2769180cbea856c57ad27ff1217d4
Viacheslav Kralin <reddismorr@gmail.com>
2025-03-20 19:00:36 +0100

    Add import for submodules

--- src/torch_fourier_filter/__init__.py
+++ src/torch_fourier_filter/__init__.py
@@ -1,10 +1,33 @@
 """Filters for image and volumes in pyTorch."""
 
-from importlib.metadata import PackageNotFoundError, version
+from importlib.metadata import PackageNotFoundError as _PackageNotFoundError
+from importlib.metadata import version as _version
 
 try:
-    __version__ = version("torch-fourier-filter")
-except PackageNotFoundError:
+    __version__ = _version("torch-fourier-filter")
+except _PackageNotFoundError:
     __version__ = "uninstalled"
 __author__ = "Josh Dickerson"
 __email__ = "jdickerson@berkeley.edu"
+
+from . import bandpass
+from . import ctf
+from . import dft_utils
+from . import dose_weight
+from . import envelopes
+from . import mtf
+from . import phase_randomize
+from . import utils
+from . import whitening
+
+__all__ = [
+    'bandpass',
+    'ctf',
+    'dft_utils',
+    'dose_weight',
+    'envelopes',
+    'mtf',
+    'phase_randomize',
+    'utils',
+    'whitening',
+]
